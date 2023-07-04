b8522c45bfd29e84a9e1317e81b375258b757e5f
Muaz Ashraf <muazashrafnet@gmail.com>
2023-07-04 21:00:29 +0500

    add loader

--- src/StoryDetails.jsx
+++ src/StoryDetails.jsx
@@ -1,78 +1,68 @@
-import { ArrowBack } from "@mui/icons-material";
 import {
-	Box,
-	Button,
-	Container,
-	ImageList,
-	ImageListItem,
-	List,
-	ListItem,
-	Typography,
-} from "@mui/material";
+  ArrowBack,
+  ArrowForward,
+  ArrowForwardIosRounded,
+} from "@mui/icons-material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const StoryDetails = () => {
-	const location = useLocation();
-	const navigate = useNavigate();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-	const { story } = location.state;
-	console.log(story);
-	if (!story) {
-		// Handle the case when the story object is null or undefined
-		return <div>No story found</div>;
-	}
+  const { story } = location.state;
+  console.log(story);
 
-	return (
-		<>
-			<Container>
-				<Typography
-					fontWeight={"bold"}
-					fontSize={"1.5rem"}
-				>
-					{story.title}
-				</Typography>
-				<Typography variant="body">{story.abstract}</Typography>
+  return (
+    <>
+      {story ? (
+        <Container>
+          <Typography fontWeight={"bold"} fontSize={"1.5rem"}>
+            {story.title}
+          </Typography>
+          <Typography variant="body">{story.abstract}</Typography>
 
-				<Typography fontStyle={"italic"}>{story.byline}</Typography>
+          <Typography fontStyle={"italic"}>{story.byline}</Typography>
 
-				<img
-					height="250px"
-					width="auto"
-					src={story.multimedia[0].url}
-				/>
+          <img height="250px" width="auto" src={story.multimedia[0].url} />
 
-				{/* {story.des_facet.map((name) => {
-					<Typography>{name}</Typography>;
-				})} */}
-				<Typography>
-					You can read the complete article here:{" "}
-					<Link
-						style={{ fontSize: "0.8rem" }}
-						to={story.url}
-					>
-						{story.url}
-					</Link>
-				</Typography>
-				<Box sx={{ display: "flex", justifyContent: "center", mt: 5 }}>
-					<Button
-						variant="contained"
-						sx={{
-							bgcolor: "black",
-							"&:hover": {
-								bgcolor: "white",
-								color: "black",
-							},
-						}}
-						startIcon={<ArrowBack />}
-						onClick={() => navigate("/")}
-					>
-						Back to Articles
-					</Button>
-				</Box>
-			</Container>
-		</>
-	);
+          <Typography>
+            You can read the complete article here:{" "}
+            <Link to={story.url}>
+              <Button
+                variant="outlined"
+                sx={{
+                  "& .MuiButton-endIcon": { transform: "rotate(-45deg)" },
+                }}
+                endIcon={<ArrowForward />}
+              >
+                NYTIMES
+              </Button>
+            </Link>
+          </Typography>
+          <Box sx={{ my: 5 }}>
+            <Button
+              variant="contained"
+              sx={{
+                bgcolor: "black",
+                "&:hover": {
+                  bgcolor: "white",
+                  color: "black",
+                },
+              }}
+              startIcon={<ArrowBack />}
+              onClick={() => navigate("/")}
+            >
+              Back to Articles
+            </Button>
+          </Box>
+        </Container>
+      ) : (
+        <p>No Story found</p>
+      )}
+    </>
+  );
 };
 
 export default StoryDetails;
--- src/TopStoriesList.jsx
+++ src/TopStoriesList.jsx
@@ -1,111 +1,117 @@
 import {
-	Box,
-	Card,
-	Container,
-	Grid,
-	MenuItem,
-	Select,
-	Stack,
-	TextField,
-	Typography,
+  Box,
+  Card,
+  CircularProgress,
+  Container,
+  Grid,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+  Typography,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const TopStoriesList = () => {
-	const navigate = useNavigate();
-	const [data, setData] = useState([]);
+  const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true); // Added loading state
 
-	const getData = async () => {
-		const response = await fetch(
-			"https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=vpPk0dSc3wlSGZUQriw78bkglP22tr2E"
-		);
-		const responseData = await response.json();
-		setData(responseData.results);
-		console.log(responseData.results);
-	};
+  const getData = async () => {
+    const response = await fetch(
+      "https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=vpPk0dSc3wlSGZUQriw78bkglP22tr2E"
+    );
+    const responseData = await response.json();
+    setData(responseData.results);
+    setLoading(false);
+    console.log(responseData.results);
+  };
 
-	useEffect(() => {
-		getData();
-	}, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
-	return (
-		<Grid
-			container
-			spacing={2}
-			style={{ display: "flex", flexWrap: "wrap" }}
-		>
-			<Grid
-				item
-				xs={6}
-			>
-				<Select
-					fullWidth
-					placeholder="Select Category"
-				>
-					<MenuItem value="World">World</MenuItem>
-					<MenuItem value="Science">Science</MenuItem>
-				</Select>
-			</Grid>
-			<Grid
-				item
-				xs={6}
-			>
-				<TextField
-					fullWidth
-					label="Search for article"
-				/>
-			</Grid>
-			{data?.map((story, index) => (
-				<Grid
-					key={index}
-					item
-					xs={6}
-					md={4}
-					lg={3}
-					sx={{ display: "flex" }}
-				>
-					<Card
-						onClick={() =>
-							navigate(`/details/${index}`, { state: { story: story } })
-						}
-						sx={{
-							flex: 1,
-							display: "flex",
-							justifyContent: "center",
+  return (
+    <Box>
+      {loading ? (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <CircularProgress color="primary" />
+        </Box>
+      ) : (
+        <Grid
+          container
+          spacing={2}
+          style={{ display: "flex", flexWrap: "wrap" }}
+        >
+          <Grid item xs={6}>
+            <Select fullWidth placeholder="Select Category">
+              <MenuItem value="World">World</MenuItem>
+              <MenuItem value="Science">Science</MenuItem>
+            </Select>
+          </Grid>
+          <Grid item xs={6}>
+            <TextField fullWidth label="Search for article" />
+          </Grid>
+          {data?.map((story, index) => (
+            <Grid
+              key={index}
+              item
+              xs={6}
+              md={4}
+              lg={3}
+              sx={{ display: "flex" }}
+            >
+              <Card
+                onClick={() =>
+                  navigate(`/details/${index}`, { state: { story: story } })
+                }
+                sx={{
+                  flex: 1,
+                  display: "flex",
+                  justifyContent: "center",
 
-							bgcolor: "#202020",
-							color: "white",
-							// py: 5,
-							px: 3,
-							border: "2px solid black",
+                  bgcolor: "#202020",
+                  color: "white",
+                  py: 2,
+                  px: 3,
+                  border: "2px solid black",
 
-							transition: "all 0.2s ease-in-out",
-							"&:hover": {
-								bgcolor: "#606060",
+                  transition: "all 0.2s ease-in-out",
+                  "&:hover": {
+                    bgcolor: "#606060",
 
-								cursor: "pointer",
-							},
-						}}
-					>
-						<Stack direction="column">
-							<Typography
-								fontWeight="bold"
-								sx={{
-									display: "flex",
-									justifyContent: "center",
-									borderBottom: "1px solid white",
-								}}
-							>
-								{index + 1}.
-							</Typography>
-							<Typography py={2}>{story.title}</Typography>
-						</Stack>
-					</Card>
-				</Grid>
-			))}
-		</Grid>
-	);
+                    cursor: "pointer",
+                  },
+                }}
+              >
+                <Stack direction="column">
+                  <Typography
+                    fontWeight="bold"
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      borderBottom: "2px solid white",
+                    }}
+                  >
+                    {index + 1}.
+                  </Typography>
+                  <Typography py={2}>{story.title}</Typography>
+                </Stack>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Box>
+  );
 };
 
 export default TopStoriesList;
