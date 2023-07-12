af6c529c1b30180602764f8b4abbef7d9273baa3
Muaz Ashraf <muazashrafnet@gmail.com>
2023-07-12 09:01:38 +0500

    apply styles to cards

--- src/SearchStories.jsx
+++ src/SearchStories.jsx
@@ -2,204 +2,233 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import {
-  Container,
-  Grid,
-  Typography,
-  Button,
-  Stack,
-  TextField,
-  Card,
+	Container,
+	Grid,
+	Typography,
+	Button,
+	Stack,
+	TextField,
+	Card,
 } from "@mui/material";
 import { ArrowBack, ArrowForward } from "@mui/icons-material";
 import Loader from "./Loader";
 import Animation from "./Animation";
 
 const SearchStories = () => {
-  const navigate = useNavigate();
-  const [data, setData] = useState(new Map());
-  const [loading, setLoading] = useState(false);
-  const [page, setPage] = useState(0);
-  const [isSubmitted, setIsSubmitted] = useState(false);
-  const [search, setSearch] = useState("");
-  const [errorMsg, setErrorMsg] = useState(null);
-  const [searchResults, setSearchResults] = useState(null);
+	const navigate = useNavigate();
+	const [data, setData] = useState(new Map());
+	const [loading, setLoading] = useState(false);
+	const [page, setPage] = useState(0);
+	const [isSubmitted, setIsSubmitted] = useState(false);
+	const [search, setSearch] = useState("");
+	const [errorMsg, setErrorMsg] = useState(null);
+	const [searchResults, setSearchResults] = useState(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setSearchResults(search);
-    setIsSubmitted(true);
-    setLoading(true);
-    setPage(0);
-    setData(new Map());
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setSearchResults(search);
+		setIsSubmitted(true);
+		setLoading(true);
+		setPage(0);
+		setData(new Map());
+	};
 
-  useEffect(() => {
-    if (isSubmitted) {
-      const getData = async () => {
-        try {
-          setErrorMsg(null);
-          if (data?.has(page)) {
-            setLoading(false);
-            return;
-          }
-          const response = await axios.get(
-            `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&page=${page}&api-key=vpPk0dSc3wlSGZUQriw78bkglP22tr2E`
-          );
-          const newData = new Map(data);
-          newData.set(page, response.data.response.docs);
-          setData(newData);
-          setLoading(false);
-          console.log(response);
-          console.log(data);
-        } catch (error) {
-          console.error("An error occurred:", error);
-          setErrorMsg(error.response);
-          setLoading(false);
-          setData(new Map());
-          setSearchResults(null);
-        }
-      };
+	useEffect(() => {
+		if (isSubmitted) {
+			const getData = async () => {
+				try {
+					setErrorMsg(null);
+					if (data?.has(page)) {
+						setLoading(false);
+						return;
+					}
+					const response = await axios.get(
+						`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&page=${page}&api-key=vpPk0dSc3wlSGZUQriw78bkglP22tr2E`
+					);
+					const newData = new Map(data);
+					newData.set(page, response.data.response.docs);
+					setData(newData);
+					setLoading(false);
+					console.log(response);
+					console.log(data);
+				} catch (error) {
+					console.error("An error occurred:", error);
+					setErrorMsg(error.response);
+					setLoading(false);
+					setData(new Map());
+					setSearchResults(null);
+				}
+			};
 
-      setLoading(true);
-      getData();
-    }
-  }, [isSubmitted, searchResults, page]);
+			setLoading(true);
+			getData();
+		}
+	}, [isSubmitted, searchResults, page]);
 
-  return (
-    <Container sx={{ mb: 4 }}>
-      <form onSubmit={handleSubmit}>
-        <Stack
-          direction={"row"}
-          spacing={2}
-          sx={{ mb: 2, justifyContent: "center" }}
-        >
-          <TextField
-            InputProps={{
-              style: { backgroundColor: "#202020", color: "white" },
-            }}
-            size="small"
-            name="search"
-            id="search"
-            placeholder="Search for articles"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <Button
-            disabled={(loading || search === "") && true}
-            sx={{ bgcolor: "black", "&:hover": { bgcolor: "#00000095" } }}
-            variant="contained"
-            type="submit"
-          >
-            Search
-          </Button>
-        </Stack>
-      </form>
-      {errorMsg && (
-        <Typography mt={5} color="red" textAlign={"center"}>
-          Status Code {errorMsg.status} - {errorMsg.statusText}
-        </Typography>
-      )}
-      {loading && isSubmitted ? (
-        <Loader />
-      ) : (
-        <Animation>
-          {searchResults && (
-            <Stack direction={"row"} alignItems={"center"} my={1} spacing={1}>
-              <Typography fontWeight={"bold"}>Search Results for:</Typography>
-              <Typography>{searchResults}</Typography>
-            </Stack>
-          )}
-          <Grid
-            container
-            spacing={2}
-            style={{ display: "flex", flexWrap: "wrap" }}
-          >
-            {!errorMsg &&
-              Array.from(data.get(page) ?? []).map((story, index) => (
-                <Grid
-                  key={index}
-                  item
-                  xs={12}
-                  sm={6}
-                  md={4}
-                  lg={3}
-                  sx={{ display: "flex" }}
-                >
-                  <Card
-                    onClick={() =>
-                      navigate(`/details/${index}`, { state: { story: story } })
-                    }
-                    sx={{
-                      flex: 1,
-                      display: "flex",
-                      justifyContent: "center",
-                      bgcolor: "#202020",
-                      color: "white",
-                      py: 2,
-                      px: 3,
-                      border: "2px solid black",
-                      transition: "all 0.2s ease-in-out",
-                      "&:hover": {
-                        bgcolor: "#606060",
-                        cursor: "pointer",
-                      },
-                    }}
-                  >
-                    <Stack direction="column">
-                      <Typography
-                        fontWeight="bold"
-                        sx={{
-                          display: "flex",
-                          justifyContent: "center",
-                          borderBottom: "2px solid white",
-                        }}
-                      >
-                        {index + 1 * page * 10 + 1}.
-                      </Typography>
-                      <Typography py={2}>{story.headline.main}</Typography>
-                    </Stack>
-                  </Card>
-                </Grid>
-              ))}
-          </Grid>
-        </Animation>
-      )}
-      {!errorMsg && isSubmitted && (
-        <Stack
-          direction={"row"}
-          mt={2}
-          display={"flex"}
-          justifyContent={"space-between"}
-          alignItems={"center"}
-        >
-          <Button
-            disabled={loading || page === 0}
-            onClick={() => {
-              setPage((prevPage) => prevPage - 1);
-              setLoading(true);
-            }}
-          >
-            <ArrowBack />
-            Previous
-          </Button>
+	return (
+		<Container sx={{ mb: 4 }}>
+			<form onSubmit={handleSubmit}>
+				<Stack
+					direction={"row"}
+					spacing={2}
+					sx={{ mb: 2, justifyContent: "center" }}
+				>
+					<TextField
+						InputProps={{
+							style: { backgroundColor: "#202020", color: "white" },
+						}}
+						size="small"
+						name="search"
+						id="search"
+						placeholder="Search for articles"
+						value={search}
+						onChange={(e) => setSearch(e.target.value)}
+					/>
+					<Button
+						disabled={(loading || search === "") && true}
+						sx={{ bgcolor: "black", "&:hover": { bgcolor: "#00000095" } }}
+						variant="contained"
+						type="submit"
+					>
+						Search
+					</Button>
+				</Stack>
+			</form>
+			{errorMsg && (
+				<Typography
+					mt={5}
+					color="red"
+					textAlign={"center"}
+				>
+					Status Code {errorMsg.status} - {errorMsg.statusText}
+				</Typography>
+			)}
+			{loading && isSubmitted ? (
+				<Loader />
+			) : (
+				<Animation>
+					{searchResults && (
+						<Stack
+							direction={"row"}
+							alignItems={"center"}
+							my={1}
+							spacing={1}
+						>
+							<Typography fontWeight={"bold"}>Search Results for:</Typography>
+							<Typography>{searchResults}</Typography>
+						</Stack>
+					)}
+					<Grid
+						container
+						spacing={2}
+						style={{ display: "flex", flexWrap: "wrap" }}
+					>
+						{!errorMsg &&
+							Array.from(data.get(page) ?? []).map((story, index) => (
+								<Grid
+									key={index}
+									item
+									xs={12}
+									sm={6}
+									md={4}
+									lg={3}
+									sx={{ display: "flex" }}
+								>
+									<Card
+										onClick={() =>
+											navigate(`/details/${index}`, { state: { story: story } })
+										}
+										sx={{
+											boxShadow: 2,
+											flex: 1,
+											display: "flex",
 
-          <Typography textAlign={"center"} fontWeight={"bold"}>
-            Page {page + 1}
-          </Typography>
-          <Button
-            disabled={loading}
-            onClick={() => {
-              setPage((prevPage) => prevPage + 1);
-              setLoading(true);
-            }}
-          >
-            Next
-            <ArrowForward />
-          </Button>
-        </Stack>
-      )}
-    </Container>
-  );
+											bgcolor: "#202020",
+											color: "white",
+
+											border: "1px solid black",
+
+											transition: "all 0.3s ease-in-out",
+											"&:hover": {
+												bgcolor: "white",
+												color: "black",
+
+												cursor: "pointer",
+											},
+										}}
+									>
+										<Stack direction="column">
+											<Typography
+												fontWeight="bold"
+												sx={{
+													display: "flex",
+													justifyContent: "center",
+													alignItems: "center",
+
+													bgcolor: "silver",
+													color: "black",
+													px: 1,
+													border: "1px solid black",
+													borderTop: "none",
+													borderLeft: "none",
+													maxWidth: "25px",
+													minWidth: "25px",
+												}}
+											>
+												{index + 1 * page * 10 + 1}.
+											</Typography>
+											<Typography
+												py={2}
+												px={3}
+											>
+												{story.headline.main}
+											</Typography>
+										</Stack>
+									</Card>
+								</Grid>
+							))}
+					</Grid>
+				</Animation>
+			)}
+			{!errorMsg && isSubmitted && (
+				<Stack
+					direction={"row"}
+					mt={2}
+					display={"flex"}
+					justifyContent={"space-between"}
+					alignItems={"center"}
+				>
+					<Button
+						disabled={loading || page === 0}
+						onClick={() => {
+							setPage((prevPage) => prevPage - 1);
+							setLoading(true);
+						}}
+					>
+						<ArrowBack />
+						Previous
+					</Button>
+
+					<Typography
+						textAlign={"center"}
+						fontWeight={"bold"}
+					>
+						Page {page + 1}
+					</Typography>
+					<Button
+						disabled={loading}
+						onClick={() => {
+							setPage((prevPage) => prevPage + 1);
+							setLoading(true);
+						}}
+					>
+						Next
+						<ArrowForward />
+					</Button>
+				</Stack>
+			)}
+		</Container>
+	);
 };
 
 export default SearchStories;
--- src/TopStoriesList.jsx
+++ src/TopStoriesList.jsx
@@ -1,16 +1,16 @@
 import {
-  Box,
-  Button,
-  Card,
-  CircularProgress,
-  Container,
-  FormControl,
-  Grid,
-  MenuItem,
-  Select,
-  Stack,
-  TextField,
-  Typography,
+	Box,
+	Button,
+	Card,
+	CircularProgress,
+	Container,
+	FormControl,
+	Grid,
+	MenuItem,
+	Select,
+	Stack,
+	TextField,
+	Typography,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
@@ -20,138 +20,153 @@ import Loader from "./Loader";
 import Animation from "./Animation";
 
 const TopStoriesList = () => {
-  const navigate = useNavigate();
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [section, setSection] = useState("home");
+	const navigate = useNavigate();
+	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [section, setSection] = useState("home");
 
-  const getData = async () => {
-    try {
-      const response = await axios.get(
-        `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=vpPk0dSc3wlSGZUQriw78bkglP22tr2E`
-      );
+	const getData = async () => {
+		try {
+			const response = await axios.get(
+				`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=vpPk0dSc3wlSGZUQriw78bkglP22tr2E`
+			);
 
-      setData(response.data.results);
-      setLoading(false);
+			setData(response.data.results);
+			setLoading(false);
 
-      console.log(response.data.results);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+			console.log(response.data.results);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  useEffect(() => {
-    setLoading(true);
-    getData();
-  }, [section]);
+	useEffect(() => {
+		setLoading(true);
+		getData();
+	}, [section]);
 
-  return (
-    <Container sx={{ mb: 4 }}>
-      <Stack
-        direction={"row"}
-        alignItems={"center"}
-        justifyContent={"center"}
-        mb={2}
-        spacing={1}
-      >
-        <Typography fontWeight={"bold"} component="label" htmlFor="cat">
-          Category:
-        </Typography>
-        <Select
-          sx={{
-            bgcolor: "black",
-            color: "white",
-            "& .MuiSvgIcon-root": {
-              color: "white",
-            },
-          }}
-          id="cat"
-          name="category"
-          size="small"
-          onChange={(e) => setSection(e.target.value)}
-          value={section}
-        >
-          <MenuItem value="home">Home</MenuItem>
-          <MenuItem value="world">World</MenuItem>
-          <MenuItem value="science">Science</MenuItem>
-        </Select>
-      </Stack>
+	return (
+		<Container sx={{ mb: 4 }}>
+			<Stack
+				direction={"row"}
+				alignItems={"center"}
+				justifyContent={"center"}
+				mb={2}
+				spacing={1}
+			>
+				<Typography
+					fontWeight={"bold"}
+					component="label"
+					htmlFor="cat"
+				>
+					Category:
+				</Typography>
+				<Select
+					sx={{
+						bgcolor: "black",
+						color: "white",
+						"& .MuiSvgIcon-root": {
+							color: "white",
+						},
+					}}
+					id="cat"
+					name="category"
+					size="small"
+					onChange={(e) => setSection(e.target.value)}
+					value={section}
+				>
+					<MenuItem value="home">Home</MenuItem>
+					<MenuItem value="world">World</MenuItem>
+					<MenuItem value="science">Science</MenuItem>
+				</Select>
+			</Stack>
 
-      <Card sx={{ p: 1, mb: 2, color: "white", bgcolor: "black" }}>
-        <Typography
-          fontWeight={"bold"}
-          textAlign={"center"}
-          fontSize={"1.5rem"}
-        >
-          {section.toUpperCase()}
-        </Typography>
-      </Card>
+			<Card sx={{ p: 1, mb: 2, color: "white", bgcolor: "black" }}>
+				<Typography
+					fontWeight={"bold"}
+					textAlign={"center"}
+					fontSize={"1.5rem"}
+				>
+					{section.toUpperCase()}
+				</Typography>
+			</Card>
 
-      {loading ? (
-        <Loader />
-      ) : (
-        <Animation>
-          <Grid
-            container
-            spacing={2}
-            style={{ display: "flex", flexWrap: "wrap" }}
-          >
-            {data
-              ?.filter((item) => item.title !== "")
-              .map((story, index) => (
-                <Grid
-                  key={index}
-                  item
-                  xs={12}
-                  sm={6}
-                  md={4}
-                  lg={3}
-                  sx={{ display: "flex" }}
-                >
-                  <Card
-                    onClick={() =>
-                      navigate(`/details/${index}`, { state: { story: story } })
-                    }
-                    sx={{
-                      flex: 1,
-                      display: "flex",
-                      justifyContent: "center",
+			{loading ? (
+				<Loader />
+			) : (
+				<Animation>
+					<Grid
+						container
+						spacing={2}
+						style={{ display: "flex", flexWrap: "wrap" }}
+					>
+						{data
+							?.filter((item) => item.title !== "")
+							.map((story, index) => (
+								<Grid
+									key={index}
+									item
+									xs={12}
+									sm={6}
+									md={4}
+									lg={3}
+									sx={{ display: "flex" }}
+								>
+									<Card
+										onClick={() =>
+											navigate(`/details/${index}`, { state: { story: story } })
+										}
+										sx={{
+											boxShadow: 2,
+											flex: 1,
+											display: "flex",
 
-                      bgcolor: "#202020",
-                      color: "white",
-                      py: 2,
-                      px: 3,
-                      border: "2px solid black",
+											bgcolor: "#202020",
+											color: "white",
 
-                      transition: "all 0.2s ease-in-out",
-                      "&:hover": {
-                        bgcolor: "#606060",
+											border: "1px solid black",
 
-                        cursor: "pointer",
-                      },
-                    }}
-                  >
-                    <Stack direction="column">
-                      <Typography
-                        fontWeight="bold"
-                        sx={{
-                          display: "flex",
-                          justifyContent: "center",
-                          borderBottom: "2px solid white",
-                        }}
-                      >
-                        {index + 1}.
-                      </Typography>
-                      <Typography py={2}>{story.title}</Typography>
-                    </Stack>
-                  </Card>
-                </Grid>
-              ))}
-          </Grid>
-        </Animation>
-      )}
-    </Container>
-  );
+											transition: "all 0.3s ease-in-out",
+											"&:hover": {
+												bgcolor: "white",
+												color: "black",
+
+												cursor: "pointer",
+											},
+										}}
+									>
+										<Stack direction="row">
+											<Typography
+												fontWeight="bold"
+												sx={{
+													display: "flex",
+													justifyContent: "center",
+													alignItems: "center",
+
+													bgcolor: "silver",
+													color: "black",
+													px: 1,
+													maxWidth: "25px",
+													minWidth: "25px",
+												}}
+											>
+												{index + 1}.
+											</Typography>
+											<Typography
+												py={2}
+												px={3}
+											>
+												{story.title}
+											</Typography>
+										</Stack>
+									</Card>
+								</Grid>
+							))}
+					</Grid>
+				</Animation>
+			)}
+		</Container>
+	);
 };
 
 export default TopStoriesList;
